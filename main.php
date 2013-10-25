3ee048b38e978e391b4c16a6882290abdd536aba
Bill Parrott <bill@chimericdream.com>
2013-10-25 09:43:24 -0500

    building state 0 works

--- main.php
+++ main.php
@@ -19,25 +19,44 @@ if (empty($outFileName)) {
 }
 $outFileName = dirname(__FILE__) . '/' . $outFileName;
 
-$baseGrammar  = buildBaseGrammar($inFileName);
-$terminals    = buildTerminalList($baseGrammar);
-$nonTerminals = buildNonTerminalList($baseGrammar);
-$augGrammar   = buildAugmentedGrammar($baseGrammar);
-$stateList    = buildStateList($augGrammar, $terminals, $nonTerminals);
+$baseGrammar = buildBaseGrammar($inFileName);
+$augGrammar  = buildAugmentedGrammar($baseGrammar);
+$stateList   = buildStateList($augGrammar);
 
-$mainOutput   = "Augmented Grammar\n-----------------\n";
-$mainOutput  .= displayGrammar($augGrammar);
-$mainOutput  .= "\nSets of LR(0) Items\n-------------------\n";
-//$mainOutput  .= displayStateList($stateList);
+$mainOutput  = "Augmented Grammar\n-----------------\n";
+$mainOutput .= displayGrammar($augGrammar);
+$mainOutput .= "\nSets of LR(0) Items\n-------------------\n";
+$mainOutput  .= displayStateList($stateList);
 
 writeOutput($mainOutput, $outFileName);
 
-function buildStateList($g, $t, $nt) {
-    do {
-        $statesAdded = false;
+function buildStateList($grammar) {
+    $states   = array();
+    $states[] = buildStateZero($grammar);
+    for ($i = 0; $i < count($states); $i++) {
+    }
+    return $states;
+}
 
-        echo "test\n";
-    } while ($statesAdded);
+function buildStateZero($grammar) {
+    $prods    = array();
+    $lhsNts   = array();
+    $lhsNts[] = $grammar[0]['rhs'];
+    for ($i = 0; $i < count($lhsNts); $i++) {
+        foreach ($grammar as $prod) {
+            if ($lhsNts[$i] == $prod['lhs']) {
+                if (strlen($prod['rhs']) == 1 && ctype_upper($prod['rhs']) && !in_array($prod['rhs'], $lhsNts)) {
+                    $lhsNts[] = $prod['rhs'];
+                }
+                $prods[] = array(
+                    'lhs'  => $prod['lhs'],
+                    'rhs'  => '@' . $prod['rhs'],
+                    'goto' => null,
+                );
+            }
+        }
+    }
+    return $prods;
 }
 
 function buildBaseGrammar($inFileName) {
@@ -59,46 +78,6 @@ function buildBaseGrammar($inFileName) {
         $grammar[] = $prod;
     }
     return $grammar;
-}
-
-function buildTerminalList($baseGrammar) {
-    $t = array();
-    foreach ($baseGrammar as $p) {
-        $lhs = str_split($p['lhs']);
-        $rhs = str_split($p['rhs']);
-        foreach ($lhs as $char) {
-            if (!ctype_upper($char) && $char != "'" && !empty($char)) {
-                $t[] = $char;
-            }
-        }
-        foreach ($rhs as $char) {
-            if (!ctype_upper($char) && !empty($char)) {
-                $t[] = $char;
-            }
-        }
-    }
-    $t = array_unique($t);
-    return $t;
-}
-
-function buildNonTerminalList($baseGrammar) {
-    $nt = array();
-    foreach ($baseGrammar as $p) {
-        $lhs = str_split($p['lhs']);
-        $rhs = str_split($p['rhs']);
-        foreach ($lhs as $char) {
-            if (ctype_upper($char) && !empty($char)) {
-                $nt[] = $char;
-            }
-        }
-        foreach ($rhs as $char) {
-            if (ctype_upper($char) && !empty($char)) {
-                $nt[] = $char;
-            }
-        }
-    }
-    $nt = array_unique($nt);
-    return $nt;
 }
 
 function buildAugmentedGrammar($grammar) {
@@ -125,6 +104,24 @@ function displayGrammar($grammar) {
     $out = '';
     foreach ($grammar as $g) {
         $out .= $g['lhs'] . '->' . $g['rhs'] . "\n";
+    }
+    return $out;
+}
+
+function displayStateList($stateList) {
+    $out = '';
+    $i   = 0;
+    foreach ($stateList as $s) {
+        $out .= "I{$i}:\n";
+        foreach ($s as $prod) {
+            $out .= '   ' . $prod['lhs'] . '->' . $prod['rhs'];
+            if (isset($prod['goto']) && !empty($prod['goto'])) {
+                $out .= str_repeat(' ', 17 - strlen($prod['rhs']));
+//                $out .= $prod['goto'];
+                $out .= '...';
+            }
+            $out .= "\n";
+        }
     }
     return $out;
 }
