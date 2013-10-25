fe87dfc8539431065a06df3a1ed385f3711bbd47
Bill Parrott <bill@chimericdream.com>
2013-10-25 12:00:16 -0500

    allllllmost there

--- main.php
+++ main.php
@@ -34,12 +34,78 @@ function buildStateList($grammar) {
     $states   = array();
     $states[] = buildStateZero($grammar);
     for ($i = 0; $i < count($states); $i++) {
+        $curr = $states[$i];
+        $s = 0;
+        foreach ($curr as &$prod) {
+            $prod['goto'] = getGotoState($prod, $curr, $states, $grammar);
+            if (!is_null($prod['goto'])) {
+                $prod['goto'] = formatGoto($prod);
+            }
+            $s++;
+        }
     }
     return $states;
 }
 
+function formatGoto($production) {
+    $prod = explode('@', $production['rhs']);
+    $char = $prod[1][0];
+    return array(
+        'char'  => $char,
+        'state' => $production['goto'],
+    );
+}
+
+function getGotoState($rule, $from, &$stateList, $grammar) {
+    $ruleParts = explode('@', $rule['rhs']);
+    if (empty($ruleParts[1])) {
+        return null;
+    }
+
+    $prods = array();
+    foreach ($from as $prod) {
+        $prodParts = explode('@', $prod['rhs']);
+        if (empty($prodParts[1])) {
+            continue;
+        }
+        if ($ruleParts[0] != $prodParts[0]) {
+            continue;
+        }
+        if ($ruleParts[1][0] == $prodParts[1][0]) {
+            $newRule = array(
+                'lhs' => $prod['lhs'],
+                'rhs' => $prodParts[0] . $prodParts[1][0] . '@' . substr($prodParts[1], 1),
+            );
+            if (!in_array($newRule, $prods)) {
+                $prods[] = $newRule;
+            }
+        }
+    }
+
+    $prods = addClosureRules($prods, $grammar);
+
+    if (!checkStateExists($prods, $stateList)) {
+        $stateList[] = $prods;
+        return count($stateList) - 1;
+    } else {
+        return array_search($prods, $stateList);
+    }
+}
+
+function checkStateExists($state, $stateList) {
+    if (in_array($state, $stateList)) {
+        return true;
+    }
+    return false;
+}
+
 function buildStateZero($grammar) {
-    $prods    = array();
+    $prods    = array(
+        array(
+            'lhs' => $grammar[0]['lhs'],
+            'rhs' => '@' . $grammar[0]['rhs'],
+        ),
+    );
     $lhsNts   = array();
     $lhsNts[] = $grammar[0]['rhs'];
     for ($i = 0; $i < count($lhsNts); $i++) {
@@ -51,12 +117,44 @@ function buildStateZero($grammar) {
                 $prods[] = array(
                     'lhs'  => $prod['lhs'],
                     'rhs'  => '@' . $prod['rhs'],
-                    'goto' => null,
                 );
             }
         }
     }
     return $prods;
+}
+
+function addClosureRules($state, $grammar) {
+    $lhsNts = array();
+    for ($i = 0; $i < count($state); $i++) {
+        $rhs = explode('@', $state[$i]['rhs']);
+        if (!isset($rhs[1]) || strlen($rhs[1]) == 0 || !ctype_upper($rhs[1][0])) {
+            continue;
+        }
+        $char = $rhs[1][0];
+        if (!in_array($char, $lhsNts)) {
+            $lhsNts[] = $char;
+        }
+    }
+
+    for ($i = 0; $i < count($lhsNts); $i++) {
+        foreach ($grammar as $prod) {
+            if ($lhsNts[$i] == $prod['lhs']) {
+                if (strlen($prod['rhs']) == 1 && ctype_upper($prod['rhs']) && !in_array($prod['rhs'], $lhsNts)) {
+                    $lhsNts[] = $prod['rhs'];
+                }
+                $prod = array(
+                    'lhs'  => $prod['lhs'],
+                    'rhs'  => '@' . $prod['rhs'],
+                );
+                if (!in_array($state, $prod)) {
+                    var_dump($prod);
+                    $state[] = $prod;
+                }
+            }
+        }
+    }
+    return $state;
 }
 
 function buildBaseGrammar($inFileName) {
@@ -117,11 +215,12 @@ function displayStateList($stateList) {
             $out .= '   ' . $prod['lhs'] . '->' . $prod['rhs'];
             if (isset($prod['goto']) && !empty($prod['goto'])) {
                 $out .= str_repeat(' ', 17 - strlen($prod['rhs']));
-//                $out .= $prod['goto'];
-                $out .= '...';
+                $out .= 'goto(' . $prod['goto']['char'] . ')=I' . $prod['goto']['state'];
             }
             $out .= "\n";
         }
+        $i++;
+        $out .= "\n";
     }
     return $out;
 }
