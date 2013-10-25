7cb70d6965bddc77a8d3e1d3c3ac10c722bece1d
Bill Parrott <bill@chimericdream.com>
2013-10-24 21:27:42 -0500

    building terminal and non terminal lists

--- main.php
+++ main.php
@@ -19,45 +19,107 @@ if (empty($outFileName)) {
 }
 $outFileName = dirname(__FILE__) . '/' . $outFileName;
 
-// Build the base grammar
-$inFile  = fopen($inFileName, 'r');
-$rawData = fread($inFile, filesize($inFileName));
-fclose($inFile);
+$baseGrammar  = buildBaseGrammar($inFileName);
+$terminals    = buildTerminalList($baseGrammar);
+$nonTerminals = buildNonTerminalList($baseGrammar);
+$augGrammar   = buildAugmentedGrammar($baseGrammar);
+$stateList    = buildStateList($augGrammar, $terminals, $nonTerminals);
 
-$grammar     = array();
-$productions = explode("\n", $rawData);
-foreach ($productions as $p) {
-    $prod = explode('->', $p);
-    $lhs  = $prod[0];
-    $rhs  = (isset($prod[1])) ? $prod[1] : null;
-    $prod = array(
-        'lhs' => $lhs,
-        'rhs' => $rhs,
-    );
-    $grammar[] = $prod;
+$mainOutput   = "Augmented Grammar\n-----------------\n";
+$mainOutput  .= displayGrammar($augGrammar);
+$mainOutput  .= "\nSets of LR(0) Items\n-------------------\n";
+//$mainOutput  .= displayStateList($stateList);
+
+writeOutput($mainOutput, $outFileName);
+
+function buildStateList($g, $t, $nt) {
+    do {
+        $statesAdded = false;
+
+        echo "test\n";
+    } while ($statesAdded);
 }
 
-// Build the augmented grammar
-$augGrammar = array();
-foreach ($grammar as $i => $p) {
-    if ($i == 0) {
-        $lhs = "'";
-        $rhs = $p['lhs'];
-    } else {
-        $lhs = $p['lhs'];
-        $rhs = $p['rhs'];
+function buildBaseGrammar($inFileName) {
+    // Build the base grammar
+    $inFile  = fopen($inFileName, 'r');
+    $rawData = fread($inFile, filesize($inFileName));
+    fclose($inFile);
+
+    $grammar     = array();
+    $productions = explode("\n", $rawData);
+    foreach ($productions as $p) {
+        $prod = explode('->', $p);
+        $lhs  = $prod[0];
+        $rhs  = (isset($prod[1])) ? $prod[1] : null;
+        $prod = array(
+            'lhs' => $lhs,
+            'rhs' => $rhs,
+        );
+        $grammar[] = $prod;
     }
-    $prod = array(
-        'lhs' => $lhs,
-        'rhs' => $rhs,
-    );
-    $augGrammar[] = $prod;
+    return $grammar;
 }
 
-echo displayGrammar($grammar);
-echo displayGrammar($augGrammar);
+function buildTerminalList($baseGrammar) {
+    $t = array();
+    foreach ($baseGrammar as $p) {
+        $lhs = str_split($p['lhs']);
+        $rhs = str_split($p['rhs']);
+        foreach ($lhs as $char) {
+            if (!ctype_upper($char) && $char != "'" && !empty($char)) {
+                $t[] = $char;
+            }
+        }
+        foreach ($rhs as $char) {
+            if (!ctype_upper($char) && !empty($char)) {
+                $t[] = $char;
+            }
+        }
+    }
+    $t = array_unique($t);
+    return $t;
+}
 
-//$outFile = fopen($outFileName, 'r');
+function buildNonTerminalList($baseGrammar) {
+    $nt = array();
+    foreach ($baseGrammar as $p) {
+        $lhs = str_split($p['lhs']);
+        $rhs = str_split($p['rhs']);
+        foreach ($lhs as $char) {
+            if (ctype_upper($char) && !empty($char)) {
+                $nt[] = $char;
+            }
+        }
+        foreach ($rhs as $char) {
+            if (ctype_upper($char) && !empty($char)) {
+                $nt[] = $char;
+            }
+        }
+    }
+    $nt = array_unique($nt);
+    return $nt;
+}
+
+function buildAugmentedGrammar($grammar) {
+    // Build the augmented grammar
+    $augGrammar = array();
+    foreach ($grammar as $i => $p) {
+        if ($i == 0) {
+            $lhs = "'";
+            $rhs = $p['lhs'];
+        } else {
+            $lhs = $p['lhs'];
+            $rhs = $p['rhs'];
+        }
+        $prod = array(
+            'lhs' => $lhs,
+            'rhs' => $rhs,
+        );
+        $augGrammar[] = $prod;
+    }
+    return $augGrammar;
+}
 
 function displayGrammar($grammar) {
     $out = '';
@@ -65,6 +127,11 @@ function displayGrammar($grammar) {
         $out .= $g['lhs'] . '->' . $g['rhs'] . "\n";
     }
     return $out;
+}
+
+function writeOutput($o, $outFileName) {
+//    $outFile = fopen($outFileName, 'r');
+    echo $o;
 }
 
 exit;
